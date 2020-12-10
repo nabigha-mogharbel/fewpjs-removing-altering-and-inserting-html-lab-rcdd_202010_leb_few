950ec9ad26682cfcf51abd67ae0df7ba08855bb4
Nabigha Mogharbel <nabiga.ba.mo1998@gmail.com>
2020-12-10 13:31:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ document.getElementById("main").remove()
 let newHeader= document.createElement("h1");
 newHeader.id="victory"
 
-newHeader.innerText='YOUR-NAME is the champion'
+newHeader.innerHTML='YOUR-NAME is the champion'
